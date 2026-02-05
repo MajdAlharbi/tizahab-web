f587f22a6a04537749ca55cd4a9310071d6c5b9a
yalsyefi <444008915@pnu.edu.sa>
2026-02-05 13:13:30 +0300

    feat: implement events list API with authentication and filtering

--- events/views.py
+++ events/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Event
+from .serializers import EventSerializer
+
+
+class EventListAPIView(ListAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Event.objects.all()
+
+        category = self.request.query_params.get('category')
+        date = self.request.query_params.get('date')
+
+        if category:
+            queryset = queryset.filter(category=category)
+
+        if date:
+            queryset = queryset.filter(date__date=date)
+
+        return queryset
